dcd6921b72cae0d13377af0409a5674c8444a380
Charan <narahc2704@gmail.com>
2026-08-05 16:05:18 +0530

    Time: 31 ms (24.46%), Space: 21.5 MB (59.93%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,7 +1,6 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums = sorted(nums)
-        i = 0
         optsum = 0
         for i in range(0,len(nums),2):
             optsum+=nums[i]
